c58c1227b1ac83566e775579e640ec7ceee054cd
shinyting <zhangting20.90@163.com>
2016-01-17 17:57:05 +0800

    绘制点状图

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -37,31 +37,81 @@ var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 box.appendChild(canvas);
 
+var Dots = [];
+
+function random(m, n) {
+	return Math.round(Math.random()*(n - m) + m) //(m,n)
+}
+function getDots () {
+	Dots = [];
+	for (var i = 0; i < size; i ++) {
+		var x = random(0, width);
+		var y = random(0, height);
+		var color = "rgb(" + random(0,255) + "," + random(0, 255) + "," + random(0, 55) + ")";
+		Dots.push({
+			x: x,
+			y: y,
+			color: color
+		});
+	}
+}
+
+var line;
 function resize () {
 	height = box.clientHeight;
 	width = box.clientWidth;
 	canvas.height = height;
 	canvas.width = width;
-	var line = ctx.createLinearGradient(0, 0, 0, height);
+	line = ctx.createLinearGradient(0, 0, 0, height);
 	line.addColorStop(0, "red");
 	line.addColorStop(0.5, "yellow");
 	line.addColorStop(1, "green");
-	ctx.fillStyle = line;
+	getDots();
 }
 
 resize();
 
 window.onresize = resize;
 
+//绘制柱状图
 function draw(arr) {
 	ctx.clearRect(0, 0, width, height);
 	var w = width / size;
+	ctx.fillStyle = line;
 	for (var i = 0; i < size; i ++) {
-		var h = arr[i] / 256 * height;
-		ctx.fillRect(w*i, height-h, w*0.6, h);
+		if (draw.type == "column") {
+			var h = arr[i] / 256 * height;
+			ctx.fillRect(w*i, height-h, w*0.6, h);
+		}
+		else if (draw.type == "dot") {
+			ctx.beginPath();
+			var o = Dots[i];
+			var r = arr[i] / 256 * 50;
+			ctx.arc(o.x, o.y, r, 0, Math.PI*2, true);
+			var g = ctx.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
+			g.addColorStop(0, "#fff");
+			g.addColorStop(1, o.color);
+			ctx.fillStyle = g;
+			ctx.fill();
+			// ctx.strokeStyle = "#fff";
+			// ctx.stroke();
+		}
+	}
+}
+draw.type = "column";
+
+var types = $("#type li");
+for (var i = 0; i < types.length; i ++) {
+	types[i].onclick = function () {
+		for (var j = 0; j < types.length; j ++) {
+			types[j].className = "";
+		}
+		this.className = "selected";
+		draw.type = this.getAttribute("data-type");
 	}
 }
 
+//向服务器请求音频数据并且播放
 function load (url) {
 	var n = ++count;
 	source && source[source.stop ? "stop" : "noteOff"]();
