4fcfa88d84749b4100cb3388a8598894af2b300d
shinyting <zhangting20.90@163.com>
2016-01-17 17:17:55 +0800

    完成音频加载解析并绘制柱状图

---
+++ public/javascripts/index.js
@@ -0,0 +1,117 @@
+function $(s) {
+	return document.querySelectorAll(s);
+}
+
+var lis = $('#list li');
+
+for (var i = 0; i < lis.length; i ++) {
+	lis[i].onclick = function () {
+		for (var j = 0; j < lis.length; j ++) {
+			lis[j].className = "";
+		}
+		this.className = "selected";
+		load("/media/"+this.title);
+	}
+}
+
+var xhr = new XMLHttpRequest();
+
+var ac = new(window.AudioContext || window.webkitAudioContext);
+//控制音量
+var gainNode = ac[ac.createGain?"createGain":"createGainNode"](0);
+gainNode.connect(ac.destination);
+
+//分析音频数据
+var analyser = ac.createAnalyser();
+var size = 128;
+analyser.fftsize = size * 2;
+analyser.connect(gainNode);
+
+var source = null;
+
+var count = 0;
+
+var box = $("#box")[0];
+var height, width;
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+box.appendChild(canvas);
+
+function resize () {
+	height = box.clientHeight;
+	width = box.clientWidth;
+	canvas.height = height;
+	canvas.width = width;
+	var line = ctx.createLinearGradient(0, 0, 0, height);
+	line.addColorStop(0, "red");
+	line.addColorStop(0.5, "yellow");
+	line.addColorStop(1, "green");
+	ctx.fillStyle = line;
+}
+
+resize();
+
+window.onresize = resize;
+
+function draw(arr) {
+	ctx.clearRect(0, 0, width, height);
+	var w = width / size;
+	for (var i = 0; i < size; i ++) {
+		var h = arr[i] / 256 * height;
+		ctx.fillRect(w*i, height-h, w*0.6, h);
+	}
+}
+
+function load (url) {
+	var n = ++count;
+	source && source[source.stop ? "stop" : "noteOff"]();
+	xhr.abort();
+	xhr.open("GET", url);
+	xhr.responseType = "arraybuffer";
+	xhr.onload = function () {
+		if (n != count) {
+			return;
+		}
+		console.log(xhr.response);
+		ac.decodeAudioData(xhr.response, function (buffer) {
+			if (n != count) {
+				return;
+			}
+			var bufferSource = ac.createBufferSource();
+			bufferSource.buffer = buffer;
+			bufferSource.connect(analyser);
+			bufferSource[bufferSource.start?"start":"noteOn"](0);
+			source = bufferSource;
+		}, function (err) {
+			console.log(err);
+		});
+	}
+	xhr.send();
+}
+
+//分析音频数据
+function visualizer () {
+	var arr = new Uint8Array(analyser.frequencyBinCount);
+	analyser.getByteFrequencyData(arr);
+	requestAnimationFrame = window.requestAnimationFrame ||
+							window.webkitrequestAnimationFrame ||
+							window.mozRequestAnimationFrame;
+	function v () {
+		analyser.getByteFrequencyData(arr);
+		requestAnimationFrame(v);
+		draw(arr);
+	}
+	requestAnimationFrame(v);
+}
+
+visualizer();
+
+function changeVolume (percent) {
+	gainNode.gain.value = percent * percent;
+}
+
+$("#volume")[0].onchange = function () {
+	changeVolume(this.value/this.max);
+}
+
+changeVolume($("#volume")[0].value/$("#volume")[0].max);
